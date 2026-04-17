2d421b1c532bfc798893a0f769fe33696009c0a0
Praj Patel <patelpraj0210@gmail.com>
2026-04-17 18:25:18 -0400

    The add Square button works adds one square everything you click "add Square"

--- squares.js
+++ squares.js
@@ -1,20 +1,31 @@
 let squareArea = document.querySelector("#squarearea");
 let squareCount = parseInt(Math.random() * 21) + 30;
+// Make some variable for a really big zIndex 
 
 for (let i = 0; i < squareCount; i++) {
+
+}
+function getRandomColor() {
+    let letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters.charAt(parseInt(Math.random() * letters.length));
+    }
+    return color;
+}
+
+function addSquare(){
     let square = document.createElement("div");
     square.className = "square";
     square.style.left = parseInt(Math.random() * 650) + "px";
     square.style.top = parseInt(Math.random() * 250) + "px";
     square.style.backgroundColor = getRandomColor();
+    
     squareArea.append(square);
 }
-function getRandomColor(){
-    let letters = "0123456789ABCDEF"; 
-    let color = "#"; 
-    for (let i = 0; i<6; i++){
-        color+=letters.charAt(parseInt(Math.random()*letters.length)); 
-    }
-    return color; 
-}
 
+function changeColors() {
+    let allSquares = document.querySelectorAll(".square");
+   
+}
+document.querySelectorAll("button")[0].onclick = addSquare;
