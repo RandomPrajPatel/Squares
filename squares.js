cffbe4c52ebd0a276c0f5f15bd67bd3dc0b08eea
Praj Patel <patelpraj0210@gmail.com>
2026-04-18 13:10:01 -0400

    Randomized the size of the squares

--- squares.js
+++ squares.js
@@ -3,7 +3,7 @@ let squareCount = parseInt(Math.random() * 21) + 30;
 let topZ = 1;
 
 for (let i = 0; i < squareCount; i++) {
-      addSquare();
+    addSquare();
 }
 function getRandomColor() {
     let letters = "0123456789ABCDEF";
@@ -14,27 +14,31 @@ function getRandomColor() {
     return color;
 }
 
-function addSquare(){
+function addSquare() {
     let square = document.createElement("div");
     square.className = "square";
     square.style.left = parseInt(Math.random() * 650) + "px";
     square.style.top = parseInt(Math.random() * 250) + "px";
     square.style.backgroundColor = getRandomColor();
-    
-square.onclick = function () {
-    if (square.style.zIndex == topZ) {
-        square.remove();
-    } else {
-        topZ++;
-        square.style.zIndex = topZ;
-    }
-};
+
+    square.onclick = function () {
+        if (square.style.zIndex == topZ) {
+            square.remove();
+        } else {
+            topZ++;
+            square.style.zIndex = topZ;
+        }
+    };
+
+    let size = parseInt(Math.random() * 11) + 45;
+    square.style.width = size + "px";
+    square.style.height = size + "px";
     squareArea.append(square);
 }
 
 function changeColors() {
     let allSquares = document.querySelectorAll(".square");
-   for (let i = 0; i < allSquares.length; i++) {
+    for (let i = 0; i < allSquares.length; i++) {
         allSquares[i].style.backgroundColor = getRandomColor();
     }
 }
